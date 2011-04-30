ca18c0049dcc3891ca91c16ef78dec7371c2bcf1
Frank Dillinger <frank.dillinger@web.de>
2011-04-30 14:35:06 +0200

    added log datei

--- media-web/delete-logs.bat
+++ media-web/delete-logs.bat
@@ -5,23 +5,23 @@ for /f "tokens=*" %%A in ( 'cd' ) do set pwd=%%A
 taskkill /im java.exe /f >nul 2>&1
 ping -n 2 localhost >nul 2>&1
  
-if exist R:\java\dev\eclipse_workspaces\default-3.3-sts\webcontracts\webcontracts-webapp goto changeDir
-rem else check if the current folder is called 'webcontracts-webapp'
+if exist C:\dev\java\workspace\default-3.3sts\media\media-web goto changeDir
+rem else check if the current folder is called 'media-web'
 goto checkForWebapp
 goto done
 
 :changeDir
 echo going to the default project webapp directory
-cd R:\java\dev\eclipse_workspaces\default-3.3-sts\webcontracts\webcontracts-webapp
+cd C:\dev\java\workspace\default-3.3sts\media\media-web
 goto deleteFiles
 
 :checkForWebapp
-echo checking if the current folder is called 'webcontracts-webapp' :
-@echo %pwd% | find "webcontracts-webapp" >nul 2>&1
+echo checking if the current folder is called 'media-web' :
+@echo %pwd% | find "media-web" >nul 2>&1
 if errorlevel 1 goto :not-found
 
 	:found
-	echo the current folder is the 'webcontracts-webapp' folder
+	echo the current folder is the 'media-web' folder
 	goto deleteFiles
 	goto done
 	
@@ -31,7 +31,7 @@ if errorlevel 1 goto :not-found
 
 :deleteFiles
 echo deleting lock and log files
-del /F/Q *.log* *.lck* *.out tm.out* *.epoch webcontracts.webapp.log.* >nul 2>&1
+del /F/Q *.log* *.lck* *.out tm.out* *.epoch media-web.webapp.log.* >nul 2>&1
 goto done
 
 :done
